62db1e226cf9531c6465b13004e0a9740065956c
oreol Noumodong <oreolnoumodong@gmail.com>
2020-05-23 03:43:19 +0100

    Initial commit

---
+++ miniCalculator/modal/index.js
@@ -0,0 +1,13 @@
+document.getElementById('myModal').innerHTML =
+    `
+
+         <!-- Modal content -->
+         
+         <div class="modal-content">
+             <img src="./modal/error.gif" alt="gig error" class="img-gif-error">
+             <span class="close">&times;</span>
+             <spanp>Ouup !</span></br>
+             <p id="messagesError">Some text in the Modal..</p>
+         </div>
+
+`
